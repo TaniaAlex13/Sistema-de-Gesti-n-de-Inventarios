a8e4f2060d2aeb0b956255c9fd5d8f7ad83d45dd
TaniaAlex13 <ta.toapantac@uea.edu.ec>
2026-02-15 17:34:24 -0500

    Agregamos el metodo para añadir productos

--- servicios/inventario.py
+++ servicios/inventario.py
@@ -4,3 +4,7 @@ class Inventario:
 
     def __init__(self):
         self.productos = []
+
+    def agregar_producto(self, producto):
+        self.productos.append(producto)
+        print("Producto agregado correctamente.")
