4324463b38b02d3ae0230343625f612a251e93fa
TaniaAlex13 <ta.toapantac@uea.edu.ec>
2026-02-15 17:36:04 -0500

    Agregamos metodo para mostrar productos

--- servicios/inventario.py
+++ servicios/inventario.py
@@ -8,3 +8,10 @@ class Inventario:
     def agregar_producto(self, producto):
         self.productos.append(producto)
         print("Producto agregado correctamente.")
+
+    def mostrar_productos(self):
+        if not self.productos:
+            print("No hay productos en el inventario.")
+        else:
+            for producto in self.productos:
+                print(producto)
